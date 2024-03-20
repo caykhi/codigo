02840b4e7ab141012814f7d507b40f73e0ea8226
caykhi <caykhi@outlook.com>
2024-03-19 23:10:40 -0300

    second commit

---
+++ codigo.c
@@ -0,0 +1,13 @@
+int main()
+{
+    int A, B, C;
+
+    printf("digite um número");
+    scanf("%d", A);
+    printf("digite um número");
+    scanf("%d", B);
+
+    C = A + B;
+
+    printf("O valor da soma é:\n", C);
+}
